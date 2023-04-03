9c51ba185edd7fad0e974cb74f08c6e61aa04c6c
Raghav <raghav.kanwal@unicommerce.com>
2023-04-04 02:02:32 +0530

    Changes

--- apis/get.ts
+++ apis/get.ts
@@ -1,18 +1,19 @@
 import { Addresses } from "../utils/interfaces";
 import gateway from './gateway';
 
-//const baseUrl = 'https://unifill.unicommerce.co.in';
+// const baseUrl = 'https://unifill.unicommerce.co.in';
 // const baseUrl = 'http://localhost:8080'; //'https://unifill.unicommerce.co.in';
 const baseUrl = "/apps/unifill";
 /********************************************** BUYER ***********************************************************/
 export async function fetchAddresses(phone: string): Promise<Addresses> {
-    // const res = await gateway(`${baseUrl}/vas/v1/addresses?mobile=${phone}`, 'GET');
+    // const res = await gateway(`${baseUrl}/v1/addresses?mobile=${phone}`, 'GET');
     // return res.json();
-    gateway(`${baseUrl}/vas/v1/addresses?mobile=${phone}`, 'GET');
+
+    gateway(`${baseUrl}/v1/addresses?mobile=${phone}`, 'GET');
 }
 
 export async function fetchAddressWithOtp(otp: string, otp_request_id: string, mobile: string): Promise<Response> {
-    const res = await gateway(`${baseUrl}/vas/v1/addresses?otp=${otp}&otp_request_id=${otp_request_id}&mobile=${mobile}`, 'GET');
+    const res = await gateway(`${baseUrl}/v1/addresses?otp=${otp}&otp_request_id=${otp_request_id}&mobile=${mobile}`, 'GET');
     // return res;
 }
 
